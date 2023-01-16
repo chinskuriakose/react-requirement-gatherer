2cfda26100ed7f41bc6a811f94d01465bbef8c31
chinskuriakose <chinskuriakose01@gmail.com>
2023-01-17 00:27:13 +0530

    react-contacts-app pushed

---
+++ req_gatherer/src/routes/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Index() {
+  return (
+    <div id='home-page'>
+        <img src="https://mobile.twitter.com/AtgeirL/photo" alt="Atgeir logo" />
+        <h1>Welcome to Requirements Portal!</h1>
+        <p>This portal can be used to create cloudformation templates based on the client input.</p>
+    </div>
+  )
+}
